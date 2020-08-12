8c05a579ebc75b585d528c225bdaa55d68eb7a88
Amogh24 <55030597+Amogh24@users.noreply.github.com>
2020-08-12 20:43:22 +0530

    unicafe done

--- exercises_part1/ex2/src/index.js
+++ exercises_part1/ex2/src/index.js
@@ -1,40 +1,59 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-const Statistics=(props)=>
-  
-    //const good=props.good
-    //const bad=props.bad
-    //const neutral=props.neutral
-    
-    (<div>
-      <p>good {props.good}</p>
-      <p> neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>   
-      <p>all {props.good +props.bad +props.neutral}</p>
-      <p>average {(props.good*1+props.bad+(-1))/(props.good+props.bad+props.neutral)}</p>
-      <p>positive {((props.good+props.neutral)/(props.good+props.bad+props.neutral))*100}%</p>
-      </div>
-  )
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [feed,setFeed]=useState(0)
   
-  
+  const HandleGood=()=>
+{
+    setGood(good+1)
+    setFeed(feed+1)
+}
+const HandleBad=()=>
+{
+  setBad(bad+1)
+  setFeed(feed+1)
+}
+const HandleNeutral=()=>
+{
+  setNeutral(neutral+1)
+  setFeed(feed+1)
+}
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={()=>setGood(good+1)}>good</button>
-      <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
-      <button onClick={()=>setBad(bad+1)}>bad</button>
+      <button onClick={HandleGood}>good</button>
+      <button onClick={HandleNeutral}>neutral</button>
+      <button onClick={HandleBad}>bad</button>
       <h1>Statistics</h1>
-     <Statistics good={good} bad={bad} neutral={neutral}/>
+     <Statistics good={good} bad={bad} neutral={neutral} feed={feed}/>
       
         
     </div>
   )
 }
+const Statistic=(props)=>{
+
+  return <tr><td>{props.text}</td><td> {props.value}</td></tr> 
+}
+const Statistics=(props)=>
+  {
+    if(props.feed>=1) 
+    {return (<div><table>
+      <Statistic text="good" value={props.good}/>
+      <Statistic text="neutral" value={props.neutral}/>
+      <Statistic text="bad" value={props.bad}/>   
+      <Statistic text="all" value= {props.good +props.bad +props.neutral}/>
+      <Statistic text="average" value= {(props.good*1+props.bad+(-1))/(props.good+props.bad+props.neutral)}/>
+      <Statistic text="positive" value= {((props.good+props.neutral)/(props.good+props.bad+props.neutral))*100}/>
+      </table> </div>
+  )}else{
+    return <div><p>No feedback given</p></div>
+  }}
+
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
